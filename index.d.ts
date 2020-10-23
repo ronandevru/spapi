4b9f6efc2dd3707f2463348dbcca772d365a6048
Ронан <orlition@gmail.com>
2020-10-23 13:55:56 +0300

    typescript support fix 1.0.5

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,31 @@
-declare module 'spapi.js' {
-   interface SPm {
-      getLastChatMessages(limit: number): Promise<ChatMessage[]>;
-      getOnlinePlayers(): Promise<{
-         players: Player[],
-         count: number;
-         max: number;
-      }>
-      getServerTime(): Promise<{
-         timeOfDay: 'DAY' | 'NIGHT',
-         ticks: number;
-         formated: string;
-      }>
-      getServerWeather(): Promise<'CLEAR' | 'RAIN' | 'THUNDER'>
-   }
-}
-
-interface Player {
-   nickname: string;
-   uuid: string;
-}
-
-interface ChatMessage {
-   sender: Player,
-   text: string;
-   time: Date;
-}
+export namespace SPm {
+    function getLastChatMessages(limit?: number): Promise<ChatMessage[]>;
+    function getOnlinePlayers(): Promise<OnlinePlayers>;
+    function getServerTime(): Promise<ServerTime>;
+    function getServerWeather(): Promise<ServerWeather>;
+ }
+ 
+ interface Player {
+    nickname: string;
+    uuid: string;
+ }
+ 
+ interface ChatMessage {
+    sender: Player,
+    text: string;
+    time: Date;
+ }
+ 
+ type ServerWeather = 'CLEAR' | 'RAIN' | 'THUNDER';
+ 
+ interface ServerTime {
+    timeOfDay: 'DAY' | 'NIGHT',
+    ticks: number;
+    formated: string;
+ }
+ 
+ interface OnlinePlayers {
+    players: Player[],
+    count: number;
+    max: number;
+ }
