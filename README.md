0127fcd1809aa8f135d4222bfc195f520791aec8
Ронан <orlition@gmail.com>
2021-02-19 16:35:52 +0300

    general improvements

--- README.md
+++ README.md
@@ -1,81 +1,110 @@
 ## SPApi.js - JS библиотека для простого доступа к api серверов #СП
+
 Поддерживаемые сервера:
+
 - #СПм
 
 ### Установка
-`npm i spapi.js` или `yarn install spapi.js`
+
+`npm i spapi.js` или `yarn add spapi.js`
+
 ### Использование
+
 ```javascript
 const spapi = require('spapi.js').SPm; //SPm - сервер #СПм.
 ```
+
 Или ES6 import
+
 ```typescript
 import { SPm as spapi } from 'spapi.js';
 ```
+
 #### Получение сообщений в чате
+
 ```javascript
-spapi.getLastChatMessages(1 /*Максимальное кол-во сообщений 1-50 (не обязательно)*/).then(messages => {
-	//messages - Массив последних сообщений
-	console.log(messages);
-}).catch(err => console.errror(err));
+spapi
+  .getLastChatMessages(1 /*Максимальное кол-во сообщений 1-50 (не обязательно)*/)
+  .then(messages => {
+    //messages - Массив последних сообщений
+    console.log(messages);
+  })
+  .catch(err => console.error(err));
 ```
+
 Пример вывода:
+
 ```json
-[{
-	"sender": {
-		"nickname": "Steve",
-		"uuid": "8667ba71-b85a-4004-af54-457a9734eed7"
-	},
-	"text": "HelloWorld",
-	"time": 1600000000
-}]
+[
+  {
+    "sender": {
+      "nickname": "Steve",
+      "uuid": "8667ba71-b85a-4004-af54-457a9734eed7"
+    },
+    "text": "HelloWorld",
+    "time": 1600000000
+  }
+]
 ```
 
 #### Получение игроков онлайн
+
 ```javascript
 spapi.getOnlinePlayers().then({ players, count, max } => {
 	//players - Массив игроков онлайн
 	//count - Кол-во игроков (= players.length)
 	//max - Максимальное кол-во игроков
 	console.log({ players, count, max });
-}).catch(err => console.errror(err));
+}).catch(err => console.error(err));
 ```
+
 Пример вывода:
+
 ```json
 {
-	"players": [{
-		"nickname": "Steve",
-		"uuid": "8667ba71-b85a-4004-af54-457a9734eed7"
-	}],
-	"count": 1,
-	"max": 69
+  "players": [
+    {
+      "nickname": "Steve",
+      "uuid": "8667ba71-b85a-4004-af54-457a9734eed7"
+    }
+  ],
+  "count": 1,
+  "max": 69
 }
 ```
 
 #### Получение времени на сервере
+
 ```javascript
-spapi.getServerTime().then({ timeOfDay, ticks, formated } => {
+spapi.getServerTime().then({ timeOfDay, ticks, formatted } => {
 	//timeOfDay - 'DAY' или 'NIGHT'
 	//ticks - Время в тиках
-	//formated - Время в 24 часовом формате
+	//formatted - Время в 24 часовом формате
 	//Подробнее про тики и т.д. - https://minecraft.gamepedia.com/Daylight_cycle#24-hour_Minecraft_day
-	console.log({ timeOfDay, ticks, formated });
-}).catch(err => console.errror(err));
+	console.log({ timeOfDay, ticks, formatted });
+}).catch(err => console.error(err));
 ```
+
 Пример вывода:
+
 ```json
 {
-	"timeOfDay": "DAY",
-	"ticks": 6000,
-	"formated": "12:00"
+  "timeOfDay": "DAY",
+  "ticks": 6000,
+  "formatted": "12:00"
 }
 ```
 
 #### Получение погоды на сервере
+
 ```javascript
-spapi.getServerWeather().then(weather => {
-	//weather - 'CLEAR', 'RAIN' или 'THUNDER'
-	console.log(weather);
-}).catch(err => console.errror(err));
+spapi
+  .getServerWeather()
+  .then(weather => {
+    //weather - 'CLEAR', 'RAIN' или 'THUNDER'
+    console.log(weather);
+  })
+  .catch(err => console.error(err));
 ```
+
 Тут и так все понятно, примеры не нужны
--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,71 @@
 import fetch from 'node-fetch';
 
-interface Player {
-   nickname: string;
-   uuid: string;
+export interface Player {
+  nickname: string;
+  uuid: string;
 }
 
-interface ChatMessage {
-   sender: Player,
-   text: string;
-   time: number;
+export interface ChatMessage {
+  sender: Player;
+  text: string;
+  time: number;
 }
 
-type ServerWeather = 'CLEAR' | 'RAIN' | 'THUNDER';
+export type ServerWeather = 'CLEAR' | 'RAIN' | 'THUNDER';
 
-interface ServerTime {
-   timeOfDay: 'DAY' | 'NIGHT',
-   ticks: number;
-   formated: string;
+export interface ServerTime {
+  timeOfDay: 'DAY' | 'NIGHT';
+  ticks: number;
+  formatted: string;
 }
 
-interface OnlinePlayers {
-   players: Player[],
-   count: number;
-   max: number;
+export interface OnlinePlayers {
+  players: Player[];
+  count: number;
+  max: number;
 }
 
 const get = async (server: string, route: string) => {
-	const res = await ( await fetch(`https://sp-api.ru/${server}/${route}`, {}) ).json();
-	if (res.error) throw new Error('Request Error');
-	return res;
+  const res = await (await fetch(`https://sp-api.ru/${server}/${route}`, {})).json();
+  if (!res.ok) throw new Error('Request Error');
+  return res;
 };
 
-class Server {
-   private name: string;
-   constructor(name: 'spm') {
-   	this.name = name;
-   }
-   getLastChatMessages = async (limit = 50): Promise<ChatMessage[]> => {
-   	const { messages } = await get(this.name, 'chat');
-   	if (messages.length > limit) messages.length = limit;
-   	for (let i = 0; i < messages.length; i++) {
-   		messages[i] = {
-   			sender: {
-   				nickname: messages[i].name,
-   				uuid: messages[i].uuid
-   			},
-   			text: messages[i].message,
-   			time: Math.round(Number(messages[i].time) / 1000)
-   		};
-   	}
-   	return messages;
-   };
-   getOnlinePlayers = async (): Promise<OnlinePlayers> => {
-   	const { players, count, max } = await get(this.name, 'online');
-   	for (let i = 0; i < players.length; i++) {
-   		players[i] = {
-   			nickname: players[i].nick,
-   			uuid: players[i].uuid
-   		};
-   	}
-   	return { players, count, max };
-   };
-   getServerTime = async (): Promise<ServerTime> => {
-   	const { time, ticks } = await get(this.name, 'time');
-   	let minutes = Math.round((ticks % 1000)*0.06).toString();
-   	if (minutes.length == 1) minutes += '0';
-   	return { timeOfDay: time, ticks, formated: `${Math.floor(ticks / 1000) + 6}:${minutes}` };
-   };
-   getServerWeather = async (): Promise<ServerWeather> => {
-   	const { weather } = await get(this.name, 'weather');
-   	return weather;
-   };
-}
-
-const SPm = new Server('spm');
-export { SPm };
+const createServer = (name: 'spm') => ({
+  getLastChatMessages: async (limit = 50): Promise<ChatMessage[]> => {
+    const { messages } = await get(name, 'chat');
+    if (messages.length > limit) messages.length = limit;
+    for (let i = 0; i < messages.length; i++) {
+      messages[i] = {
+        sender: {
+          nickname: messages[i].name,
+          uuid: messages[i].uuid
+        },
+        text: messages[i].message,
+        time: Math.round(Number(messages[i].time) / 1000)
+      };
+    }
+    return messages;
+  },
+  getOnlinePlayers: async (): Promise<OnlinePlayers> => {
+    const { players, count, max } = await get(name, 'online');
+    for (let i = 0; i < players.length; i++) {
+      players[i] = {
+        nickname: players[i].nick,
+        uuid: players[i].uuid
+      };
+    }
+    return { players, count, max };
+  },
+  getServerTime: async (): Promise<ServerTime> => {
+    const { time, ticks } = await get(name, 'time');
+    let minutes = Math.round((ticks % 1000) * 0.06).toString();
+    if (minutes.length === 1) minutes += '0';
+    return { timeOfDay: time, ticks, formatted: `${Math.floor(ticks / 1000) + 6}:${minutes}` };
+  },
+  getServerWeather: async (): Promise<ServerWeather> => {
+    const { weather } = await get(name, 'weather');
+    return weather;
+  }
+});
+export const SPm = createServer('spm');
